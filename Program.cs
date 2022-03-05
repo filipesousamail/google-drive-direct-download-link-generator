e1032d3b0328bdf856935a3ec2c8976bfcf8bee4
Filipe Sousa <filipesousamail@gmail.com>
2022-03-05 16:22:17 +0000

    tidying up

--- Program.cs
+++ Program.cs
@@ -2,11 +2,6 @@
 using System.Text.RegularExpressions;
 
 /*
-Code is NOT production ready and is prodiced without any warranty, use it at your own risk.
-Feel free to open a PR to enhance it, though
-
-Credits to this project/page: https://sites.google.com/site/gdocs2direct/
-
 Google drive provided link: https://drive.google.com/file/d/1aTmvp0aoJ0ZHZRn8gmtZDWmV-imRSHja/view
 Final downloadable link: https://drive.google.com/uc?export=download&id=1aTmvp0aoJ0ZHZRn8gmtZDWmV-imRSHja
 */
@@ -15,7 +10,7 @@ var fileLink = "https://drive.google.com/file/d/1aTmvp0aoJ0ZHZRn8gmtZDWmV-imRSHj
 var fileId = Regex.Matches(fileLink, "/file/d/(.+)/(.+)").FirstOrDefault()?.Groups[1].Value;
 var downloadLink = $"https://drive.google.com/uc?export=download&id={fileId}";
 
-Console.WriteLine("Wayyyy, we've got ourselves another donwload link!!!");
+Console.WriteLine("Yayyyy, we've got ourselves another donwload link!!!");
 
 using var httpClient = new HttpClient();
 var httpResponse = await httpClient.GetAsync(downloadLink);
